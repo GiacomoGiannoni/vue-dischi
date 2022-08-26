286b902872e9d36ef8a2c8c79877f4eefa59560e
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-08-26 17:04:31 +0200

    completamento header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-
+    <MyHeader/>
   </div>
 </template>
 
 <script>
-
+import MyHeader from './components/MyHeader';
 
 export default {
   name: 'App',
+  components: {
+    MyHeader,
 
+  }
 }
 </script>
 
 <style lang="scss">
-
+@import '@/styles/general.scss'
 
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div class="Top">
+    <span>Logo</span>
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+    @import '@/styles/vars.scss';
+    .Top {
+        background-color: $color_top;
+        width: 100%;
+        height: 50px;
+    }
 
+    span {
+        color: white;
+        line-height: 50px;
+        margin-left: 20px;
+    }
 </style>
